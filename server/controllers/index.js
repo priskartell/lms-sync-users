df74b24b8840e2da5d8ca40c9bb759279aaf74c6
Emil Stenberg <esandin@gmail.com>
2016-11-08 14:05:53 +0100

    removed a little more unused code

--- server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = {
-  // Do not remove the System controller!
-  System: require('./systemCtrl'),
-
-  // Replace with actual API controller
-  Sample: require('./sampleCtrl')
-}
