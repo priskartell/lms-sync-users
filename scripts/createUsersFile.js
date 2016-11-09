1f26e3c887a4fe5eef07fde69cf80629dc943779
Emil Stenberg <esandin@gmail.com>
2016-11-09 15:06:03 +0100

    writing all employees to file

--- scripts/createUsersFile.js
+++ scripts/createUsersFile.js
@@ -1,36 +1,49 @@
 const ldap = require('ldapjs')
 const config = require('../server/init/configuration')
+const fs = require('fs')
 
+const fileName = 'allUsers.csv'
+const headers = ['user_id', 'login_id', 'email', 'full_name', 'status']
+const attributes = ['ugKthid', 'ugUsername', 'mail', 'email_address', , 'name']
+function escapeCsvData (str) {
+  return str
+}
+
+function writeLine (strArr, _fileName = fileName) {
+  const line = strArr.map(escapeCsvData).join(',') + '\n'
+  fs.appendFile(_fileName, line, (err) => {
+    if (err) throw err
+  })
+}
+
+fs.unlinkSync(fileName)
 const client = ldap.createClient({
   url: config.secure.ldap.client.url
 })
 
+writeLine(headers, fileName)
+
 const opts = {
-  filter: 'objectClass=user',
+  filter: 'ugAffiliation=employee',
+  // filter: 'ugAffiliation=student',
   scope: 'sub',
   paged: true,
-  sizeLimit: 200
-  // attributes: ['dn', 'sn', 'cn']
+  sizeLimit: 1000,
+  attributes
 }
-console.log(config.secure.ldap.username)
-client.bind(config.secure.ldap.bind.username, config.secure.ldap.bind.password, function (err) {
-  console.log('connected,', err)
 
+client.bind(config.secure.ldap.bind.username, config.secure.ldap.bind.password, function (err) {
   client.search('OU=UG,DC=ug,DC=kth,DC=se', opts, function (err, res) {
     res.on('searchEntry', function (entry) {
-      // process.stdout.write('.')
-
-      console.log('entry: ' + JSON.stringify(entry.object, null, 4))
-      client.unbind()
-    })
-    res.on('searchReference', function (referral) {
-      console.log('referral: ' + referral.uris.join())
+      console.log(entry.object)
+      const o = entry.object
+      writeLine([o.ugKthid, `${o.ugUsername}@kth.se`, o.mail, o.name, 'active' ])
     })
     res.on('error', function (err) {
       console.error('error: ' + err.message)
     })
     res.on('end', function (result) {
-      console.log('status: ' + result.status)
+      console.log('End!')
       client.unbind()
     })
   })
