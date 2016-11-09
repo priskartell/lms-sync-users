7d2c485c1d2dfcce3382dc0bfa0790cf5bf2b90b
Emil Stenberg <esandin@gmail.com>
2016-11-09 15:19:44 +0100

    write file for all users

--- scripts/createUsersFile.js
+++ scripts/createUsersFile.js
@@ -4,7 +4,8 @@ const fs = require('fs')
 
 const fileName = 'allUsers.csv'
 const headers = ['user_id', 'login_id', 'email', 'full_name', 'status']
-const attributes = ['ugKthid', 'ugUsername', 'mail', 'email_address', , 'name']
+const attributes = ['ugKthid', 'ugUsername', 'mail', 'email_address', 'name']
+
 function escapeCsvData (str) {
   return str
 }
@@ -23,28 +24,29 @@ const client = ldap.createClient({
 
 writeLine(headers, fileName)
 
-const opts = {
-  filter: 'ugAffiliation=employee',
-  // filter: 'ugAffiliation=student',
-  scope: 'sub',
-  paged: true,
-  sizeLimit: 1000,
-  attributes
-}
-
 client.bind(config.secure.ldap.bind.username, config.secure.ldap.bind.password, function (err) {
-  client.search('OU=UG,DC=ug,DC=kth,DC=se', opts, function (err, res) {
-    res.on('searchEntry', function (entry) {
-      console.log(entry.object)
-      const o = entry.object
-      writeLine([o.ugKthid, `${o.ugUsername}@kth.se`, o.mail, o.name, 'active' ])
-    })
-    res.on('error', function (err) {
-      console.error('error: ' + err.message)
-    })
-    res.on('end', function (result) {
-      console.log('End!')
-      client.unbind()
+  ['employee', 'student'].forEach(type => {
+    const opts = {
+      filter: `ugAffiliation=${type}`,
+      scope: 'sub',
+      paged: true,
+      sizeLimit: 1000
+      // attributes
+    }
+
+    client.search('OU=UG,DC=ug,DC=kth,DC=se', opts, function (err, res) {
+      res.on('searchEntry', function (entry) {
+        // console.log(entry.object)
+        // console.log('.')
+        const o = entry.object
+        writeLine([o.ugKthid, `${o.ugUsername}@kth.se`, o.mail, o.name, 'active' ])
+      })
+      res.on('error', function (err) {
+        console.error('error: ' + err.message)
+      })
+      res.on('end', function (result) {
+        console.log('Done with ', type)
+      })
     })
   })
 })
