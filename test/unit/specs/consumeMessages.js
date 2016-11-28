1edc7fa3ad11b8bb3928fb3e91c4c24e80df0d7b
Emil Stenberg <emilsten@kth.se>
2016-11-28 13:56:39 +0100

    two passing tests

--- test/unit/specs/consumeMessages.js
+++ test/unit/specs/consumeMessages.js
@@ -2,40 +2,37 @@
  require('rewire-global').enable()
  const sinon = require('sinon')
  const proxyquire = require('proxyquire').noCallThru()
+ let body
 
- test('read message without body should not fail', t => {
+ const queue = sinon.stub().returns({
+   readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: ''}))
+ })
+
+ const consumeMessages = proxyquire('../../../consumeMessages.js', {
+   'node-queue-adapter': queue,
+   './deleteMessage': sinon.stub().returns(Promise.resolve()),
+   './handleMessage': sinon.stub().returns(Promise.resolve())
+ })
+
+// overwrite original function to be able to break the eternal loop in the test
+ const readMessage = consumeMessages.readMessage
+ const readMessageStub = sinon.stub()
+ consumeMessages.__set__('readMessage', readMessageStub)
+
+
+ test('read message without body should call read message again', t => {
    t.plan(1)
-   const queue = sinon.stub().returns({
-     readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: '{}'}))
-   })
 
-   const consumeMessages = proxyquire('../../../consumeMessages.js', {
-     'node-queue-adapter': queue,
-     './deleteMessage': sinon.stub().returns(Promise.resolve()),
-     './handleMessage': sinon.stub().returns(Promise.resolve())
-   })
-  // overwrite original function to be able to break the eternal loop in the test
-   const readMessage = consumeMessages.readMessage
-   const readMessageStub = sinon.stub()
-   consumeMessages.__set__('readMessage', readMessageStub)
 
-   // call the original function
    readMessage()
   .then(res => t.equal(readMessageStub.called, true))
  })
 
- // test.skip('read message with body should...', function (t) {
- //   const queue = sinon.stub().returns({
- //     readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: '{}'}))
- //   })
- //
- //   const consumeMessages = proxyquire('../../../consumeMessages.js', {
- //     'node-queue-adapter': queue
- //   })
- //
- //   const readMessage = consumeMessages.__get__('readMessage')
- //
- //   t.plan(1)
- //
- //   t.equal(1, 1)
- // })
+ test('read message with incorrect body should call read message again', t => {
+   t.plan(1)
+   body = ''
+
+   readMessage()
+  .then(res => t.equal(readMessageStub.called, true)
+  )
+ })
