2e2ae1ba3990fdbcf7d68994b1f42612bbda8a2a
Emil Stenberg <emilsten@kth.se>
2016-11-28 13:43:08 +0100

    linting

--- test/unit/specs/consumeMessages.js
+++ test/unit/specs/consumeMessages.js
@@ -1,40 +1,41 @@
  var test = require('tape')
-require('rewire-global').enable()
-const sinon = require('sinon')
-const proxyquire = require('proxyquire').noCallThru()
+ require('rewire-global').enable()
+ const sinon = require('sinon')
+ const proxyquire = require('proxyquire').noCallThru()
 
-test('read message without body should not fail', t => {
-  t.plan(1)
-  const queue = sinon.stub().returns({
-    readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: '{}'}))
-  })
+ test('read message without body should not fail', t => {
+   t.plan(1)
+   const queue = sinon.stub().returns({
+     readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: '{}'}))
+   })
 
-  const consumeMessages = proxyquire('../../../consumeMessages.js', {
-    'node-queue-adapter': queue,
-    './deleteMessage': sinon.stub().returns(Promise.resolve()),
-    './handleMessage': sinon.stub().returns(Promise.resolve())
-  })
+   const consumeMessages = proxyquire('../../../consumeMessages.js', {
+     'node-queue-adapter': queue,
+     './deleteMessage': sinon.stub().returns(Promise.resolve()),
+     './handleMessage': sinon.stub().returns(Promise.resolve())
+   })
   // overwrite original function to be able to break the eternal loop in the test
-  const readMessage = consumeMessages.readMessage
-  const readMessageStub = sinon.stub()
-  consumeMessages.__set__('readMessage', readMessageStub)
+   const readMessage = consumeMessages.readMessage
+   const readMessageStub = sinon.stub()
+   consumeMessages.__set__('readMessage', readMessageStub)
 
-  consumeMessages.readMessage()
+   // call the original function
+   readMessage()
   .then(res => t.equal(readMessageStub.called, true))
-})
+ })
 
-test.skip('read message with body should...', function (t) {
-  const queue = sinon.stub().returns({
-    readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: '{}'}))
-  })
-
-  const consumeMessages = proxyquire('../../../consumeMessages.js', {
-    'node-queue-adapter': queue
-  })
-
-  const readMessage = consumeMessages.__get__('readMessage')
-
-  t.plan(1)
-
-  t.equal(1, 1)
-})
+ // test.skip('read message with body should...', function (t) {
+ //   const queue = sinon.stub().returns({
+ //     readMessageFromQueue: sinon.stub().returns(Promise.resolve({body: '{}'}))
+ //   })
+ //
+ //   const consumeMessages = proxyquire('../../../consumeMessages.js', {
+ //     'node-queue-adapter': queue
+ //   })
+ //
+ //   const readMessage = consumeMessages.__get__('readMessage')
+ //
+ //   t.plan(1)
+ //
+ //   t.equal(1, 1)
+ // })
