79d66957c66a225066c88db73eacca3b7eab8484
Emil Stenberg <emilsten@kth.se>
2016-11-28 12:22:57 +0100

    removed test'

--- test/unit/specs/consumeMessages.js
+++ test/unit/specs/consumeMessages.js
@@ -1,18 +1,7 @@
 var test = require('tape')
 
-test('timing test', function (t) {
-  t.plan(2)
+test('timing test', function(t) {
+  t.plan(1)
 
-  t.equal(typeof Date.now, 'function')
-
-  // Om jag förstår rätt så är estet nedan
-  // baserat på cpu hastighet. Vilket gör att
-  // expected: 100 kommer bero på yttre omständigheter.
-  /*
-  var start = Date.now()
-
-  setTimeout(function () {
-    t.equal(Date.now() - start, 100)
-  }, 100)
-  */
+  t.equal(1, 1)
 })
