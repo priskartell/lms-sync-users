07785ba8e64b8d6e5eef9a4332e45bcd7bb83714
Emil Stenberg <emilsten@kth.se>
2016-11-29 11:50:01 +0100

    added tests for deleting messages

--- test/unit/specs/consumeMessages.js
+++ test/unit/specs/consumeMessages.js
@@ -5,13 +5,15 @@ const sinon = require('sinon')
 const proxyquire = require('proxyquire').noCallThru()
 
 const readMessageFromQueueStub = sinon.stub()
+const deleteMessageFromQueue = sinon.stub()
+
 const queue = sinon.stub().returns({
-  readMessageFromQueue: readMessageFromQueueStub
+  readMessageFromQueue: readMessageFromQueueStub,
+  deleteMessageFromQueue
 })
 
 const consumeMessages = proxyquire('../../../messages/consumeMessages', {
   'node-queue-adapter': queue,
-  './deleteMessage': sinon.stub().returns(Promise.resolve()),
   './handleMessage': sinon.stub().returns(Promise.resolve())
 })
 
@@ -20,39 +22,62 @@ const readMessage = consumeMessages.readMessage
 const readMessageStub = sinon.stub()
 consumeMessages.__set__('readMessage', readMessageStub)
 
+function reset() {
+  readMessageFromQueueStub.reset()
+  deleteMessageFromQueue.reset()
+}
+
 test('read message without body should call read message again', t => {
-  t.plan(1)
+  t.plan(2)
+  reset()
   readMessageFromQueueStub.returns(Promise.resolve({}))
 
   readMessage()
-    .then(res => t.equal(readMessageStub.called, true))
+    .then(res => {
+      t.equal(readMessageStub.called, true)
+      t.equal(deleteMessageFromQueue.called, false)
+    })
 })
 
 test('read message without message should call read message again', t => {
-  t.plan(1)
+  t.plan(2)
+  reset()
+
   readMessageFromQueueStub.returns(Promise.resolve())
 
   readMessage()
-    .then(res => t.equal(readMessageStub.called, true))
+    .then(res => {
+      t.equal(readMessageStub.called, true)
+      t.equal(deleteMessageFromQueue.called, false)
+    })
 })
 
 test('read message with incorrect body should call read message again', t => {
-  t.plan(1)
+  t.plan(2)
+  reset()
+
   readMessageFromQueueStub.returns(Promise.resolve({
     body: ''
   }))
 
   readMessage()
-    .then(res => t.equal(readMessageStub.called, true)
-  )
+    .then(res => {
+      t.equal(readMessageStub.called, true)
+      t.equal(deleteMessageFromQueue.called, false) // its ok to keep these messages. They will eventually be moved to the error queue where they will be reported as incorrect messages
+    })
 })
 
 test('read message with correct body should call read message again', t => {
-  t.plan(1)
+  t.plan(2)
+  reset()
+
   readMessageFromQueueStub.returns(Promise.resolve({
     body: '{}'
   }))
 
   readMessage()
-    .then(res => t.equal(readMessageStub.called, true))
+    .then(res => {
+      t.equal(readMessageStub.called, true)
+      t.equal(deleteMessageFromQueue.called, true)
+    })
 })
