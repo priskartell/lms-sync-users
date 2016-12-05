bd693597e84912200ca65ba1130ca17538f7a32d
Emil Stenberg <emilsten@kth.se>
2016-12-05 12:03:15 +0100

    mock node-queue-adapter

--- test/unit/consumeMessages/readMessageUnlessReading.js
+++ test/unit/consumeMessages/readMessageUnlessReading.js
@@ -2,10 +2,13 @@
 var test = require('tape')
 require('rewire-global').enable()
 const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+const consumeMessages = proxyquire('../../../messages/consumeMessages', {'node-queue-adapter': sinon.stub().returns({})})
+
 
 test('if it is already reading, then it should just return ', t => {
   t.plan(1)
-  const consumeMessages = require('../../../messages/consumeMessages')
   const readMessage = sinon.stub()
   consumeMessages.__set__('readMessage', readMessage)
   consumeMessages.__set__('isReading', true)
