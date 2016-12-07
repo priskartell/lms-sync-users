4099d91f1e12465afafd6e7d4d1742a7f3314dd9
Emil Stenberg <emilsten@kth.se>
2016-12-07 10:31:35 +0100

    lint

--- test/unit/consumeMessages/readMessage.js
+++ test/unit/consumeMessages/readMessage.js
@@ -1,15 +1,14 @@
 var test = require('tape')
 require('rewire-global').enable()
 const sinon = require('sinon')
-const proxyquire = require('proxyquire')
 
-test('should abort promise chain if no message is read', t=>{
+test('should abort promise chain if no message is read', t => {
   t.plan(1)
-  const consumeMessages = require('../../../messages/consumeMessages.js');
-  const queue = {readMessageFromQueue:sinon.stub().returns(Promise.resolve())}
+  const consumeMessages = require('../../../messages/consumeMessages.js')
+  const queue = {readMessageFromQueue: sinon.stub().returns(Promise.resolve())}
   consumeMessages.__set__('queue', queue)
 
   consumeMessages.__get__('readMessage')()
 
-  t.equal(1,0)
+  t.equal(1, 0)
 })
