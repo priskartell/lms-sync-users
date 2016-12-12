74f0a465bf853a9deee388e074ecc0d394ead997
Emil Stenberg <emilsten@kth.se>
2016-12-12 13:15:10 +0100

    created script

--- test/unit/consumeMessages/readMessage.js
+++ test/unit/consumeMessages/readMessage.js
@@ -2,13 +2,12 @@ var test = require('tape')
 require('rewire-global').enable()
 const sinon = require('sinon')
 
-test.skip('should abort promise chain if no message is read', t => {
+test.only('should abort promise chain if no message is read', t => {
   t.plan(1)
   const consumeMessages = require('../../../messages/consumeMessages.js')
   const queue = {readMessageFromQueue: sinon.stub().returns(Promise.resolve())}
   consumeMessages.__set__('queue', queue)
 
   consumeMessages.__get__('readMessage')()
-
   t.equal(1, 0)
 })
