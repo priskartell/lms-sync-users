3511f8c362686e25dc0a6b938ecb0b0172cc4d80
Emil Stenberg <esandin@gmail.com>
2016-12-13 13:56:40 +0100

    only include courses in p3

--- scripts/createCoursesFile.js
+++ scripts/createCoursesFile.js
@@ -3,12 +3,30 @@ const Promise = require('bluebird') // use bluebird to get a little more promise
 const parseString = Promise.promisify(require('xml2js').parseString)
 const _ = require('lodash')
 
-const term = '2017:1'
-// const period = '3'
+const constants = {
+  term:'2017:1',
+  period:'3'
+}
 
+function get (url) {
+  console.log(url)
+  return rp({
+    url,
+    method: 'GET',
+    json: true,
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+}
+
+/***
+* return example:
+* {"round":{"courseCode":"MJ2244","startTerm":"20171","roundId":"1","xmlns":""},"periods":[{"term":"20171","number":"3"}]}
+*/
 function addPeriods (courseRounds) {
   function addInfoForCourseRound ([round]) {
-    return get(`http://www.kth.se/api/kopps/v1/course/${round.courseCode}/round/${term}/${round.roundId}`)
+    return get(`http://www.kth.se/api/kopps/v1/course/${round.courseCode}/round/${constants.term}/${round.roundId}`)
     .then(parseString)
     .then(roundInfo => {
       const periods = roundInfo.courseRound.periods && roundInfo.courseRound.periods[0].period.map(period => period.$)
@@ -27,37 +45,15 @@ function filterCoursesByCount (courseRounds, filterFn) {
   .filter(filterFn)
 }
 
-function get (url) {
-  console.log(url)
-  return rp({
-    url,
-    method: 'GET',
-    json: true,
-    headers: {
-      'content-type': 'application/json'
-    }
-  })
-}
+// Start executing
 
-get(`http://www.kth.se/api/kopps/v1/courseRounds/${term}`)
+get(`http://www.kth.se/api/kopps/v1/courseRounds/${constants.term}`)
 .then(parseString)
 .then(courseRounds => courseRounds.courseRoundList.courseRound)
 .then(courseRounds => courseRounds.map(round => round.$))
 .then(courseRounds => filterCoursesByCount(courseRounds, courses => courses.length === 1))
 .then(addPeriods)
+// .then(arg => console.log('arg', JSON.stringify(arg[0])))
+.then(coursesWithPeriods => coursesWithPeriods.filter(({periods}) => periods && periods.find(({number}) => number === constants.period)))
 .then(arg => console.log('arg', JSON.stringify(arg[0])))
-// .then(filterCoursesByPeriod)
 .catch(e => console.error(e))
-// .then(courseRounds => c)
-// .then(arg => console.log('arg', JSON.stringify( arg[0] )))
-
-    // .then(departments => Promise.map(departments, dep => rp({
-    //   url: `https://www.kth.se/api/kopps/v2/courses/${dep.code}.json`,
-    //   method: 'GET',
-    //   json: true,
-    //   headers: {
-    //     'content-type': 'application/json'
-    //   }
-    // })
-    // ))
-    // .then(coursesPerDepartment => coursesPerDepartment.forEach(dep => coursesForSchool.push(...dep.courses)))
