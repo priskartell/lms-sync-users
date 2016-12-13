95a624e1c7e4d78baed3228bc69849173dbdbae3
Emil Stenberg <esandin@gmail.com>
2016-12-13 10:22:51 +0100

    lint

--- scripts/createCoursesFile.js
+++ scripts/createCoursesFile.js
@@ -1,10 +1,10 @@
 const rp = require('request-promise')
 const Promise = require('bluebird') // use bluebird to get a little more promise functions then the standard Promise AP
 const parseString = Promise.promisify(require('xml2js').parseString)
-const _ = require('lodash');
+const _ = require('lodash')
 
-function filterCoursesByCount(courseRounds, filterFn){
-  const courseRoundsGrouped = _.groupBy(courseRounds, courseRound=>courseRound.courseCode)
+function filterCoursesByCount (courseRounds, filterFn) {
+  const courseRoundsGrouped = _.groupBy(courseRounds, courseRound => courseRound.courseCode)
 
   return Object.getOwnPropertyNames(courseRoundsGrouped)
   .map(name => courseRoundsGrouped[name])
@@ -24,7 +24,7 @@ rp({
 .then(courseRounds => courseRounds.courseRoundList.courseRound)
 .then(courseRounds => courseRounds.map(round => round.$))
 .then(courseRounds => filterCoursesByCount(courseRounds, courses => courses.length > 1))
-.then(arg => console.log('arg', JSON.stringify( arg )))
+.then(arg => console.log('arg', JSON.stringify(arg)))
     // .then(departments => Promise.map(departments, dep => rp({
     //   url: `https://www.kth.se/api/kopps/v2/courses/${dep.code}.json`,
     //   method: 'GET',
