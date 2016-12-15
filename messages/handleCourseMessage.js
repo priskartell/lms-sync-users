4571e752f1e5b576fd5d39212d7c4b3acff5742e
Payam Madjidi <payam.madjidi@gmail.com>
2016-12-15 10:51:58 +0100

    removing latest development on retriving users from canvas

--- messages/handleCourseMessage.js
+++ messages/handleCourseMessage.js
@@ -14,77 +14,34 @@ const csvDir = config.secure.localFile.csvDir
 const lmsDatabase = config.secure.azure.databaseName
 const lmsCollection = config.secure.azure.collectionName
 
-function _createCsvFile (msg, sisCourseCode, enrollmentsArray, timeStamp) {
+function _createCsvFile (msg, sisCourseCode, timeStamp) {
   let header = 'course_id,user_id,role,status\n'
   let msgtype = msg._desc.userType
   let csvFileName = 'enrollments.' + msgtype + '.' + sisCourseCode + '.' + timeStamp + '.csv'
   let msgFileName = 'enrollments.' + msgtype + '.' + sisCourseCode + '.' + timeStamp + '.msg'
   let csvString = ''
-  let msgUsers = new Set([])
-  let canvasUsers = new Set([])
-  let canvasUserArray = []
-  let csvArray = []
-  let csvDarray = []
-
-  log.info('Number of users recived from canvas: ' + enrollmentsArray.length)
-  log.info('Number of users recieved from message: ' + msg.member.length)
 
   if (msg.member && msg.member.length > 0) {
-    msgUsers = new Set(msg.member)
+    let csvArray = msg.member.map(user => {
+      return {
+        course_id: sisCourseCode,
+        user_id: user,
+        role: msgtype,
+        status: 'active'
+      }
+    })
+    csvArray.forEach(csvRow => {
+      csvString = csvString + `${csvRow.course_id},${csvRow.user_id},${csvRow.role},${csvRow.status}
+`
+    })
   }
-  canvasUserArray = enrollmentsArray.map(canvasUser => canvasUser.sis_user_id)
 
-  canvasUserArray.forEach(item => log.info('Debug canvas userid: ' + item))
-  msg.member.forEach(item => log.info('debug msg userid: ' + item))
-
-  if (canvasUserArray && canvasUserArray.length > 0) {
-    canvasUsers = new Set(canvasUserArray)
-  }
-  let activateSet = new Set([...msgUsers].filter(user => !canvasUsers.has(user)))
-  let deactivateSet = new Set([...canvasUsers].filter(user => !msgUsers.has(user)))
-  let enrolleCounter = 0
-  let unrolleCounter = 0
-  csvArray = [...activateSet].map(user => {
-    enrolleCounter += 1
-    log.info('[' + enrolleCounter + '] ' + 'User: ' + user + ' will be enrolled to course: ' + sisCourseCode)
-    return {
-      course_id: sisCourseCode,
-      user_id: user,
-      role: msgtype,
-      status: 'active'
-    }
-  })
-
-  csvDarray = [...deactivateSet].map(user => {
-    unrolleCounter += 1
-    log.info('[' + unrolleCounter + '] ' + 'User: ' + user + ' should be unrolled from course ' + sisCourseCode + ' ,no action taken for now: ')
-    return {
-      course_id: sisCourseCode,
-      user_id: user,
-      role: msgtype,
-      status: 'deactivate'
-    }
-  })
-  csvDarray = [] // Just to shut up the linter....
-  log.info(csvDarray) // Just to shut up the linter....
-  csvArray.forEach(csvRow => {
-    csvString = csvString + `${csvRow.course_id},${csvRow.user_id},${csvRow.role},${csvRow.status}\n`
-  })
   let csvData = header + csvString
   log.info('\nGoing to open file: ' + csvFileName + ' ' + msgFileName)
-
-  if (csvString === '') {
-    csvFileName = csvFileName + '.EMPTY'
-  }
-
   return cl.cloudStoreTextToFile(csvFileName, csvVol, csvData)
   .then(result => { log.info(result); return cl.cloudStoreTextToFile(msgFileName, msgVol, JSON.stringify(msg, null, 4)) })
   .then(() => cl.cloudgetFile(csvFileName, csvVol, csvDir))
-  .then(result => {
-    log.info(result)
-    let fileName = csvDir + result.name
-    return {fileName: fileName, enrollments: enrolleCounter}
-  })
+  .then(result => { log.info(result); return {csvContent: csvData, csvFileName: csvDir + result.name} })
   .catch(error => { log.error(error); return Promise.reject(error) })
 }
 
@@ -107,9 +64,9 @@ function _parseKeyStudent (key) {
     year = myArray[yearIn]
     ladok = myArray[ladokIn]
     let sisCourseCode = course + termin + year + ladok
-    return Promise.resolve(sisCourseCode)
+    return sisCourseCode
   }
-  return Promise.reject(Error('_parseKeyStudent, ' + key + ' could not be decoded'))
+  return
 }
 
 function _parseKeyTeacher (key) {
@@ -122,6 +79,7 @@ function _parseKeyTeacher (key) {
   let terminIn = 5
   let yearIn = 4
   let ladokIn = 6
+
   let myRe = /^edu.courses.(\w+).(\w+).(\d\d)(\d\d)(\d).(\d).(\w+)$/g
   let myArray = myRe.exec(key)
   if (myArray != null) {
@@ -130,135 +88,66 @@ function _parseKeyTeacher (key) {
     year = myArray[yearIn]
     ladok = myArray[ladokIn]
     let sisCourseCode = course + termin + year + ladok
-    return Promise.resolve(sisCourseCode)
+    return sisCourseCode
   }
-  return Promise.reject(Error('_parseKeyTeacher, ' + key + ' could not be decoded'))
+  return
 }
 
 function _parseKey (key, msgtype) {
+  let sisCourseCode = 0
   if (msgtype === type.students) {
-    return _parseKeyStudent(key)
+    sisCourseCode = _parseKeyStudent(key)
   }
   if (msgtype === type.teachers || msgtype === type.assistants) {
-    return _parseKeyTeacher(key)
-  } else {
-    return Promise.reject(Error('_parseKey unkown type, ' + msgtype))
+    sisCourseCode = _parseKeyTeacher(key)
   }
-}
-
-function _getEnrollmentsForCourse (canvasCourseId, msgtype) {
-  let enrollType = ''
-  switch (msgtype) {
-    case type.students:
-      enrollType = 'StudentEnrollment'
-      break
-    case type.teachers:
-      enrollType = 'TeacherEnrollment'
-      break
-    case type.assistants:
-      enrollType = 'TaEnrollment'
-      break
-    default:
-      log.warn('enrollment type not defined....')
-      return Promise.reject(Error('_getEnrollmentsForCourse, Invalid message type: ' + msgtype))
+  if (!sisCourseCode) {
+    log.error('\nCourse code not parsable from Key. type, ' + msgtype + ' key, ' + key)
+    return Promise.reject(Error('Key parse error, type, ' + msgtype + ' key, ' + key))
   }
-  return canvasApi.getEnrollmentList(canvasCourseId, enrollType)
-}
-
-function _sendCsvIfNotEmpty (fileName) {
-  if (fileName.split('.')[6] === 'EMPTY') {
-    log.info('CSV file is empty, will not be sent to canvas, skipping....')
-    return
-  } else {
-    return canvasApi.sendCsvFile(fileName)
-  }
-}
-
-function _generateReport (csvTicket, sisCourseCode, timeStamp, document, timeInMinutes) {
-  let milsecTime = timeInMinutes * 60000
-  let documentId = sisCourseCode + '.' + timeStamp
-  document['id'] = documentId
-  if (!csvTicket) {
-    // No need to wait for canvas, no file was sent
-    document['csvTicket'] = 'Empty'
-    document['csvReport'] = 'Empty'
-    document['FINALSTATUS']  = "SUCCESS"
-    console.info(JSON.stringify(document, null, 4))
-    let collectionUrl = `dbs/${lmsDatabase}/colls/${lmsCollection}`
-    return cl.cloudCreateDocument(document, collectionUrl)
-  } else {
-    setTimeout(_createReport.bind(null, sisCourseCode, timeStamp, document), milsecTime)
-    return Promise.resolve('Done')
-  }
-}
-
-function _createReport (sisCourseCode, timeStamp, document) {
-  let tmp = JSON.parse(document.csvTicket)
-  document['csvTicket'] = tmp
-  return canvasApi.getCsvFileStatus(tmp.id).then(result => {
-    document['csvReport'] = JSON.parse(result)
-    let enrollments = parseInt(document.enrollments)
-    let canvasenrollments = parseInt(document.csvReport.data.counts.enrollments)
-    log.debug("validator enrollments, MSG: " + enrollments + " CANVAS: " + canvasenrollments)
-    if ( enrollments  != canvasenrollments) {
-      log.warn("CSV IMPORT NOT COMPLETE....")
-      document['FINALSTATUS'] = "FAILED"
-    }
-    if (enrollments === canvasenrollments) {
-      log.info("CSV IMPORT COMPLETE.......")
-      document['FINALSTATUS']  = "SUCCESS"
-    }
-    console.info(JSON.stringify(document, null, 4))
-    let collectionUrl = `dbs/${lmsDatabase}/colls/${lmsCollection}`
-    return cl.cloudCreateDocument(document, collectionUrl)
-  })
-}
-
-function _handleError (err, sisCourseCode) {
-  if (err.statusCode === 404) {
-    log.info('Course does not exist in canvas, skipping, '.red + sisCourseCode.red)
-    return Promise.resolve('Course does not exist in canvas')
-  } else {
-    return Promise.reject(Error(err))
-  }
+  return Promise.resolve(sisCourseCode)
 }
 
 function _process (msg) {
   let sisCourseCode = ''
   let timeStamp = Date.now()
-  let msgtype = msg._desc.userType
-  let key = msg.ug1Name
-  let document = {}
 
-  return _parseKey(key, msgtype)
+  return _parseKey(msg.ug1Name, msg._desc.userType)
     .then(sisCode => {
       sisCourseCode = sisCode
-      log.info(`In _process ${sisCourseCode}, processing for ${msgtype}`)
-      document['sisCourseCode'] = sisCourseCode
+      log.info(`In _process ${sisCourseCode}, processing for ${msg._desc.userType}`)
       return canvasApi.findCourse(sisCourseCode)
     })
     .then(result => {
-      let Result = JSON.parse(result)
-      document['canvasCourse'] = Result
-      return _getEnrollmentsForCourse(Result.id, msgtype)
-    })
-    .then(enrollmentsArray => {
-      let tmpArray = enrollmentsArray.map(canvasUser => canvasUser.sis_user_id)
-      document['usersInCanvas'] = tmpArray.sort()
-      document['usersInmsg'] = msg.member.sort()
-      return _createCsvFile(msg, sisCourseCode, enrollmentsArray, timeStamp)
+      log.info(JSON.parse(result, null, 4))
+      // Here to drag out list of enrollments from canvas and compare it
+      // with the content of the message based on user type
+      // This is necessary to understand what the message content really mean,
+      // if the message is activate or deactivation enrollments
+      // Investigate if there is a chance if the message both contains
+      // activation and deactivation, for now pressumed unlikekly
+      // return canvasApi.getEnrollmentList(sisCourseCode)})
+      return _createCsvFile(msg, sisCourseCode, timeStamp)
     })
     .then(csvObject => {
-      document['fileName'] = csvObject.fileName
-      document['enrollments'] = csvObject.enrollments
-      return _sendCsvIfNotEmpty(csvObject.fileName)
+      log.info('FileName: ', csvObject.csvFileName)
+      return csvObject.csvFileName
     })
-    .then(csvTicket => {
-      document['csvTicket'] = csvTicket
-      let waitMinutes = 1
-      return _generateReport(csvTicket, sisCourseCode, timeStamp, document, waitMinutes)
+    .then(fileName => canvasApi.sendCsvFile(fileName))
+    .then(canvasReturnValue => {
+      let documentId = sisCourseCode + '.' + timeStamp
+      let document = {id: documentId, msg: msg, resp: canvasReturnValue}
+      let collectionUrl = `dbs/${lmsDatabase}/colls/${lmsCollection}`
+      return cl.cloudCreateDocument(document, collectionUrl)
     })
-    .catch(err => _handleError(err, sisCourseCode))
+    .catch(err => {
+      if (err.statusCode === 404) {
+        log.info('Course does not exist in canvas, skipping, '.red + sisCourseCode.red)
+        return Promise.resolve('Course does not exist in canvas')
+      } else {
+        return Promise.reject(Error(err))
+      }
+    })
 }
 
 module.exports = function (msg, counter) {
