449fa387fb2a5298bdab1197b7caf172450f52e3
Emil Stenberg <emilsten@kth.se>
2016-12-16 13:32:17 +0100

    writing teachers

--- scripts/createEnrollmentsFile.js
+++ scripts/createEnrollmentsFile.js
@@ -1,18 +1,6 @@
-/*
-[
-    "courseRound": {
-      "courseCode": "MJ2244",
-      "startTerm": "20171",
-      "roundId": "1",
-      "startWeek": "2017-03",
-      "endWeek": "2017-11",
-      "xmlns": "http://www.kth.se/student/kurser"
-
-*/
 const Promise = require('bluebird')
 const ldap = require('ldapjs')
 const fs = Promise.promisifyAll(require('fs'))
-
 const config = require('../server/init/configuration')
 
 const client = ldap.createClient({
@@ -31,44 +19,11 @@ const fileName = `csv/enrollments-${constants.term}-${constants.period}.csv`
 const coursesFileName = `csv/courses-${constants.term}-${constants.period}.csv`
 
 const columns = [
-  'course_id',
-  'short_name',
-  'long_name',
-  'start_date',
-  'account_id',
-  'status']
-
-function writeUsersForCourse ({course, courseRound, constants}) {
-  console.log('get users for the course', courseRound.courseCode)
-  return Promise.map(['teachers', 'assistants', 'courseresponsible'], type => {
-    const courseInitials = courseRound.courseCode.substring(0, 2)
-    return clientAsync.searchAsync('OU=UG,DC=ug,DC=kth,DC=se', {
-      scope: 'sub',
-      filter: `(&(objectClass=group)(CN=edu.courses.${courseInitials}.${courseRound.courseCode}.${courseRound.startTerm}.${courseRound.roundId}.${type}))`,
-      timeLimit: 11,
-      paged: true
-    })
-  .then(res => new Promise((resolve, reject) => {
-    res.on('searchEntry', ({object}) => resolve(object.member))
-    res.on('end', ({object}) => resolve(object && object.member))
-    res.on('error', reject)
-  }))
-  .then(member => {
-    // Always use arrays as result
-    if (typeof member === 'string') {
-      return [member]
-    } else {
-      return member || []
-    }
-  })
-  })
-  .then(arrayOfMembers => Promise.map(arrayOfMembers, getUsersForMembers))
-  .then(() => console.log('TODO: write a line!'))
-  // .then(([teachers, assistants, courseresponsible]) => {
-  //   // console.log(`got ${teachers.length} teachers, ${assistants.length} assistants and ${courseresponsible.length} courseresponsible for course ${courseRound.courseCode}`);
-  //   return {sisCourseId: course.course.sis_course_id, users: {teachers, assistants, courseresponsible}}
-  // })
-}
+  'course_id', //	text 	Required field if section_id is missing. The course identifier from courses.csv
+  'user_id', //	text 	Required field. The User identifier from users.csv
+  'role', // 	text 	Required field if role_id missing. student, teacher, ta, observer, designer, or a custom role defined by the account
+  'status' //	Required field. active, deleted, completed, inactive
+]
 
 function getUsersForMembers (members) {
   return Promise.map(members, member => {
@@ -95,14 +50,53 @@ function getUsersForMembers (members) {
   .then(userArray => [].concat.apply([], userArray))
 }
 
-// module.exports = {
-//   createFile (arrayOfCourseInfo) {
-//     fileName = `csv/enrollments-${constants.term}-${constants.period}.csv`
-    fs.unlinkAsync(fileName)
+function writeUsersForCourse ([sis_course_id, courseCode, name, start_date, account_id]) {
+  return Promise.map(['teachers', 'assistants', 'courseresponsible'], type => {
+    const courseInitials = courseCode.substring(0, 2)
+    const startTerm = constants.term.replace(':', '')
+    const roundId = sis_course_id.substring(sis_course_id.length - 1, sis_course_id.length)
+
+    return clientAsync.searchAsync('OU=UG,DC=ug,DC=kth,DC=se', {
+      scope: 'sub',
+      filter: `(&(objectClass=group)(CN=edu.courses.${courseInitials}.${courseCode}.${startTerm}.${roundId}.${type}))`,
+      timeLimit: 11,
+      paged: true
+    })
+    .then(res => new Promise((resolve, reject) => {
+      res.on('searchEntry', ({object}) => resolve(object.member))
+      res.on('end', ({object}) => resolve(object && object.member))
+      res.on('error', reject)
+    }))
+    .then(member => {
+      // Always use arrays as result
+      if (typeof member === 'string') {
+        return [member]
+      } else {
+        return member || []
+      }
+    })
+  })
+    .then(arrayOfMembers => Promise.map(arrayOfMembers, getUsersForMembers))
+    .then(([teachers, assistants, courseresponsible]) => {
+      console.log(JSON.stringify(teachers, null, 4))
+      //  {
+    //     "dn": "CN=Nenad Glodic (glodic),OU=EMPLOYEES,OU=USERS,OU=UG,DC=ug,DC=kth,DC=se",
+    //     "controls": [],
+    //     "name": "Nenad Glodic (glodic)",
+    //     "ugKthid": "u15nm7pe"
+    // }
+      return Promise.map(teachers, user => csvFile.writeLine([sis_course_id, user.ugKthid, 'teacher', 'active'], fileName))
+      // console.log(`got ${teachers.length} teachers, ${assistants.length} assistants and ${courseresponsible.length} courseresponsible for course ${courseCode}`)
+    })
+}
+
+fs.unlinkAsync(fileName)
     .catch(e => console.log('couldnt delete file. It probably doesnt exist.', e.message))
     .then(() => clientAsync.bindAsync(config.secure.ldap.bind.username, config.secure.ldap.bind.password))
     .then(() => csvFile.writeLine(columns, fileName))
-    // .then(() => Promise.map(arrayOfCourseInfo, writeUsersForCourse))
+    .then(() => fs.readFileAsync(coursesFileName, 'utf8'))
+    .then(fileContentStr => fileContentStr.split('\n'))
+    .then(lines => lines.splice(1, lines.length - 2)) // first line is columns, last is new empty line
+    .then(lines => lines.map(line => line.split(','))) // split into values per column
+    .then(linesArrays => Promise.map(linesArrays, writeUsersForCourse))
     .finally(() => clientAsync.unbindAsync())
-//   }
-// }
