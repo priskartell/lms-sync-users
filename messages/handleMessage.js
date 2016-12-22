37ceceb01920c81377950fa249deee8f56abc443
Emil Stenberg <emilsten@kth.se>
2016-12-22 14:04:39 +0100

    re-enabled handleCourseMessage again

--- messages/handleMessage.js
+++ messages/handleMessage.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const handleCourseMessage = require('./handleCourseMessage')
+const handleCourseMessage = require('./handleCourseMessage')
 const handleUserMessage = require('./handleUserMessage')
 const {type} = require('message-type')
 const log = require('../server/init/logging')
@@ -10,8 +10,8 @@ module.exports = function (msg) {
   if (msg._desc.type === type.course) {
     log.info('This is a message about a course. Skip it.')
     return Promise.resolve(msg)
-    // log.info('Started handling message to update a course info...'.green)
-    // return handleCourseMessage(msg)
+    log.info('Started handling message to update a course info...'.green)
+    return handleCourseMessage(msg)
   } else if (msg._desc.type === type.user) {
     log.info('Started handling the queue message to create/update a user...'.green)
     return handleUserMessage(msg)
