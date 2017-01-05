fe092979b5cad986caa67785e94cc8cfc016f121
Emil Stenberg <emilsten@kth.se>
2017-01-05 12:03:37 +0100

    building first draft

---
+++ test/unit/azureStorage.js
@@ -0,0 +1,8 @@
+const test = require('tape')
+
+// const proxyquire = require('proxyquire').noCallThru()
+test.only('should require without connecting to azure', t => {
+  // const azureStorage = require('../../azureStorage.js')
+  // t.plan(1)
+  // t.ok(azureStorage)
+})
