9909d7547e0585947e446b3f9694330861bc30f2
Emil Stenberg <emilsten@kth.se>
2017-01-09 16:47:09 +0100

    create a queue, post a message, read it and delete the queue

--- test/integration/user.js
+++ test/integration/user.js
@@ -9,12 +9,16 @@ console.log('TODO: SHOULD start docker image before tests run!')
 // docker run --name canvas-docker -p 3000:3000 lbjay/canvas-docker
 
 // Overwrite to use an empty queue
-config.full.azure.queueName = 'lms-sync-integration-tests'
+config.full.azure.queueName = 'lms-sync-integration-tests-' + new Date().getTime()
 
 test.only('should create a message on the queue and then read the same message', t => {
   t.plan(1)
   const message = {1:2}
-  queue.sendQueueMessage(config.full.azure.queueName, message)
+  console.log('using qeue:', config.full.azure.queueName)
+
+  queue.createQueueIfNotExists(config.full.azure.queueName)
+  .then(()=>queue.sendQueueMessage(config.full.azure.queueName, message))
   .then(()=>consumeMessages.readMessage())
   .then(res=> t.assert(res.body, message))
+  .then(()=> queue.deleteQueue(config.full.azure.queueName))
 })
