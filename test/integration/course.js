b6fa11d1fe4930d0811dbb8057622c1de664b85d
Emil Stenberg <emilsten@kth.se>
2017-01-17 16:11:57 +0100

    using a message for the same course as just created

--- test/integration/course.js
+++ test/integration/course.js
@@ -6,21 +6,23 @@ const randomstring = require('randomstring')
 test.only('should enroll an assistant in an existing course in canvas', t => {
   t.plan(1)
   let canvasCourse
+
+  const courseCode = 'A' + randomstring.generate(5)
+
   const message = {
     'kthid': 'u2kub9j5',
     'ugClass': 'group',
     'deleted': false,
-    'ug1Name': 'edu.courses.SF.SF1625.20171.1.assistants',
+    'ug1Name': `edu.courses.SF.${courseCode}.20171.1.assistants`,
     'name_en': 'Teacher assistants for SF1625 Spring 17 1',
     'name_sv': 'Lärarassistenter på SF1625 VT17 1',
     'member': ['u1znmoik']}
 
-  const courseCode = 'A' + randomstring.generate(5)
 
   const course = {
     'name': 'Emil testar',
     'course_code': courseCode,
-    'sis_course_id': `${courseCode}HT161`,
+    'sis_course_id': `${courseCode}VT171`,
     'start_at': '2016-08-29T09:15:03.346Z'
   }
 
