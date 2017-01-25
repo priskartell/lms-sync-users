f23fb9ad903fd6d20f439431a8e294b4cdca4bc2
Emil Stenberg <emilsten@kth.se>
2017-01-25 12:00:28 +0100

    added one more test for create csvFile

--- test/unit/createCsvFile.test.js
+++ test/unit/createCsvFile.test.js
@@ -1,6 +1,7 @@
 const test = require('tape')
 require('rewire-global')
-test.only('should only create the header when message has no members', t => {
+
+test('should only create the header when message has no members', t => {
   t.plan(1)
   const createCsvFile = require('../../messages/createCsvFile')
   const azureStorage = createCsvFile.__get__('azureStorage')
@@ -14,7 +15,34 @@ test.only('should only create the header when message has no members', t => {
   }
 
   createCsvFile(message, 'SF1626VT171', '/tmp/', 'dev-lms-csv').then(result => {
+    // TODO: should this really return the name from azure?
     t.deepEqual(result, {csvContent: 'course_id,user_id,role,status\n', csvFileName: '/tmp/a cloud file name'})
+  })
+})
+
+test.only('should create the header and one more line when message has one member', t => {
+  t.plan(1)
+  const createCsvFile = require('../../messages/createCsvFile')
+
+  const azureStorage = createCsvFile.__get__('azureStorage')
+  azureStorage.cloudStoreTextToFile = ()=>Promise.resolve()
+  azureStorage.cloudgetFile = ()=>Promise.resolve({name:'a cloud file name'})
+
+  const sisCourseCode = 'SF1626VT171'
+
+  const message = {
+    _desc: {
+      userType: 'students',
+    },
+    member: ['abc123']
+  }
+
+  createCsvFile(message, sisCourseCode, '/tmp/', 'dev-lms-csv').then(result => {
+    // TODO: should this really return the name from azure?
+    const expectedCsvContent = `course_id,user_id,role,status
+${sisCourseCode},abc123,students,active
+`
+    t.deepEqual(result, {csvContent: expectedCsvContent, csvFileName: '/tmp/a cloud file name'})
   })
 })
 
