0f43ed29d9375ee9260f6080a27da8e3ab9f2600
Emil Stenberg <emilsten@kth.se>
2017-01-25 11:01:53 +0100

    test passe

--- test/unit/calcSisForOmregistrerade.test.js
+++ test/unit/calcSisForOmregistrerade.test.js
@@ -3,4 +3,5 @@ test.only('should do something',t =>{
   const calcSisForOmregistrerade = require('../../messages/calcSisForOmregistrerade');
   const result = calcSisForOmregistrerade({})
   t.ok(result)
+  t.end()
 })
