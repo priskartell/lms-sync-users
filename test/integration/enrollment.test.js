e6cd38aa28778776583f9abf065d1572918a028a
Emil Stenberg <emilsten@kth.se>
2017-01-26 11:00:00 +0100

    fixed error in test

--- test/integration/enrollment.test.js
+++ test/integration/enrollment.test.js
@@ -3,6 +3,16 @@ const {handleMessages} = require('././utils')
 const canvasApi = require('../../canvasApi')
 const randomstring = require('randomstring')
 
+function processMessage (message, course) {
+  // First create a fresch course in canvas
+  return canvasApi.createCourse({course}, 14) // Courses that starts with an 'A' is handled by account 14
+  .then(res => { canvasCourse = res })
+  .then(() => handleMessages(message))
+  .then(([{resp}]) => canvasApi.pollUntilSisComplete(resp.id))
+  .then(() => canvasApi.getEnrollments(canvasCourse.id))
+  .then(([enrolledUser]) => enrolledUser)
+}
+
 test('should enroll an assistant in an existing course in canvas', t => {
   t.plan(1)
   let canvasCourse
@@ -20,41 +30,13 @@ test('should enroll an assistant in an existing course in canvas', t => {
     'sis_course_id': `${courseCode}VT171`
   }
 
-  // First create a fresch course in canvas
-  canvasApi.createCourse({course}, 14) // Courses that starts with an 'A' is handled by account 14
-  .then(res => { canvasCourse = res })
-  .then(() => handleMessages(message))
-  .then(([{resp}]) => canvasApi.pollUntilSisComplete(resp.id))
-  .then(() => canvasApi.getEnrollments(canvasCourse.id))
-  .then(([enrolledUser]) => {
+  processMessage(message, course)
+  .then((enrolledUser) => {
     t.equal(enrolledUser.sis_user_id, userKthId)
   })
 })
 
-test('should enroll a student to a course in canvas', t => {
-  t.plan(1)
-  t.equal(1, 0)
-})
-
-function handleMessage({message, courseCode0, courseCode1, userKthId}){
-  let canvasCourse
-
-  const course = {
-    name: 'Emil testar',
-    'course_code': courseCode0 + courseCode1,
-    'sis_course_id': `${courseCode0 + courseCode1}VT173`
-  }
-
-  // First create a fresch course in canvas
-  return canvasApi.createCourse({course}, 14) // Courses that starts with an 'A' is handled by account 14
-  .then(res => { canvasCourse = res })
-  .then(() => handleMessages(message))
-  .then(([{resp}]) => canvasApi.pollUntilSisComplete(resp.id))
-  .then(() => canvasApi.getEnrollments(canvasCourse.id))
-  .then(([enrolledUser]) => enrolledUser)
-}
-
-test.only('should enroll a re-registered student in an existing course in canvas', t => {
+test('should enroll a re-registered student in an existing course in canvas', t => {
   t.plan(2)
   const userKthId = 'u1znmoik'
   const courseCode0 = 'A' + randomstring.generate(1)
@@ -65,14 +47,20 @@ test.only('should enroll a re-registered student in an existing course in canvas
     ug1Name: `ladok2.kurser.${courseCode0}.${courseCode1}.omregistrerade_20171`,
     member: [userKthId]}
 
-  handleMessage({message, courseCode0, courseCode1, userKthId})
+  const course = {
+    name: 'Emil testar',
+    'course_code': courseCode0 + courseCode1,
+    'sis_course_id': `${courseCode0 + courseCode1}VT173`
+  }
+
+  processMessage(message, course)
   .then(enrolledUser => {
     t.ok(enrolledUser)
     t.equal(enrolledUser.sis_user_id, userKthId)
   })
 })
 
-test('should enroll a student in an existing course in canvas', t => {
+test.only('should enroll a student in an existing course in canvas', t => {
   t.plan(2)
   let canvasCourse
 
@@ -89,16 +77,11 @@ test('should enroll a student in an existing course in canvas', t => {
   const course = {
     name: 'Emil testar',
     'course_code': courseCode0 + courseCode1,
-    'sis_course_id': `${courseCode0 + courseCode1}VT173`
+    'sis_course_id': `${courseCode0 + courseCode1}VT171`
   }
 
-  // First create a fresch course in canvas
-  canvasApi.createCourse({course}, 14) // Courses that starts with an 'A' is handled by account 14
-  .then(res => { canvasCourse = res })
-  .then(() => handleMessages(message))
-  .then(([{resp}]) => canvasApi.pollUntilSisComplete(resp.id))
-  .then(() => canvasApi.getEnrollments(canvasCourse.id))
-  .then(([enrolledUser]) => {
+  processMessage(message, course)
+  .then((enrolledUser) => {
     t.ok(enrolledUser)
     t.equal(enrolledUser.sis_user_id, userKthId)
   })
