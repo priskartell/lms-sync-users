c7a1d97e189f696dbe45ab4f539cfb9ea3178e72
Emil Stenberg <emilsten@kth.se>
2017-01-26 14:25:53 +0100

    using kth-canvas-api from npm

--- canvasApi.js
+++ canvasApi.js
@@ -1,6 +1,6 @@
 const config = require('./server/init/configuration')
 const logger = require('kth-node-log')
-const CanvasApi = require('canvas-api')
+const CanvasApi = require('kth-canvas-api')
 const canvasApi = new CanvasApi(config.full.canvas.apiUrl, config.secure.canvas.apiKey)
 
 canvasApi.logger = logger
