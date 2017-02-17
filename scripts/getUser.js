3e8717818f00b4804aa7bde3cc751fee08b73e33
Emil Stenberg <emilsten@kth.se>
2017-02-17 11:01:54 +0100

    created script to list users not in csv file

--- scripts/getUser.js
+++ scripts/getUser.js
@@ -1,5 +1,5 @@
 process.env['NODE_ENV'] = 'development'
- require('kth-node-log').init()
+require('kth-node-log').init()
 const canvasApi = require('../canvasApi')
 canvasApi.getUser('u1znmoik')
 .then(arg => console.log(JSON.stringify(arg)))
--- scripts/listUsersInCanvasButNotMembers.js
+++ scripts/listUsersInCanvasButNotMembers.js
@@ -5,7 +5,7 @@ const canvasApi = new CanvasApi(config.full.canvas.apiUrl, config.secure.canvas.
 require('colors')
 const ldap = require('ldapjs')
 const Promise = require('bluebird')
-const {without} = require('lodash');
+const {without} = require('lodash')
 const attributes = ['ugKthid', 'ugUsername']
 
 const ldapClient = Promise.promisifyAll(ldap.createClient({
@@ -26,15 +26,15 @@ const ugUsers = ldapClient.bindAsync(config.secure.ldap.bind.username, config.se
   const result = []
   res.on('searchEntry', ({object}) => result.push(object.ugKthid))
   res.on('error', reject)
-  res.on('end', ()=>resolve(result))
+  res.on('end', () => resolve(result))
 }))
-.then(ugUsers=>{
+.then(ugUsers => {
   ldapClient.unbindAsync()
   return ugUsers
 })
 
 Promise.all([canvasApi.listUsers().map(canvasUser => canvasUser.sis_user_id), ugUsers])
 // .then(([canvasUsers, ugUsers])=>console.log(canvasUsers, ugUsers))
-.then(([canvasUsers, ugUsers])=>without(canvasUsers, ...ugUsers))
+.then(([canvasUsers, ugUsers]) => without(canvasUsers, ...ugUsers))
 .then(canvasUsersNotInUg => console.log(canvasUsersNotInUg.length))
 .catch(e => console.error(e))
