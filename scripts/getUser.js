cc6a06dbe8f08eac6309f7d2f3a29eaca899b466
Emil Stenberg <emilsten@kth.se>
2017-02-17 11:00:09 +0100

    created script to list users not in csv file

--- scripts/getUser.js
+++ scripts/getUser.js
@@ -1,4 +1,6 @@
+process.env['NODE_ENV'] = 'development'
+ require('kth-node-log').init()
 const canvasApi = require('../canvasApi')
-canvasApi.getUser('aoeu')
-.then(arg => console.log('arg', JSON.stringify(arg)))
+canvasApi.getUser('u1znmoik')
+.then(arg => console.log(JSON.stringify(arg)))
 .catch(e => console.error('error', e.statusCode))
