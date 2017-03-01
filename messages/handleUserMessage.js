abc1f5939f3de5c1f3d5620e3974fc4b4a343034
Elena Rakhimova <elenara@kth.se>
2017-03-01 17:06:37 +0100

    Added communication channel

--- messages/handleUserMessage.js
+++ messages/handleUserMessage.js
@@ -23,8 +23,12 @@ function convertToCanvasUser (msg) {
         sis_user_id: msg.kthid // CSVs analogi av 'user_id' needed for enrollments
       },
       user: {
-        'name': `${msg.given_name} ${msg.family_name}`,
-        'email': msg.primary_email
+        'name': `${msg.given_name} ${msg.family_name}`
+        // 'email': msg.primary_email
+      },
+      communication_channel: {
+        type: 'email',
+        address: msg.primary_email
       }
     }
     return user
