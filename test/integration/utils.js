a9386eee4d0f1c4d905d1112a954f6a77040a28d
Emil Stenberg <emilsten@kth.se>
2017-03-06 14:03:53 +0100

    refactor test

--- test/integration/utils.js
+++ test/integration/utils.js
@@ -3,42 +3,39 @@ const Promise = require('bluebird')
 const rewire = require('rewire')
 const consumeMessages = rewire('../../messages/consumeMessages')
 
+function sendAndWaitUntilMessageProcessed (message) {
+  console.log('Send and read a message', message)
+  const resultPromise = new Promise((resolve, reject) => {
+    consumeMessages.eventEmitter.once('messageProcessed', (msg, result) => {
+      resolve(result)
+    })
+  })
+
+  queue.sendQueueMessage(config.full.azure.queueName, message)
+  .catch(err => console.error(err))
+
+  return resultPromise
+}
+
 function handleMessages (...messages) {
   console.log('handle messages', messages.length)
   config.full.azure.queueName = 'lms-sync-integration-tests-' + Math.random().toString(36)
   config.secure.azure.queueName = config.full.azure.queueName
 
-  let receiver
-
-  function sendAndWaitUntilMessageProcessed (message) {
-    console.log('Send and read a message', message)
-    const resultPromise = new Promise((resolve, reject) => {
-      consumeMessages.eventEmitter.once('messageProcessed', (msg, result) => {
-        resolve(result)
-      })
-    })
-
-    queue.sendQueueMessage(config.full.azure.queueName, message)
-    .catch(err => console.error(err))
-
-    return resultPromise
-  }
-
-  let result
+  let receiver, result
+  
   return queue.createQueueIfNotExists(config.full.azure.queueName)
   .then(() => consumeMessages.start())
   .then(_receiver => { receiver = _receiver })
   .then(() => Promise.mapSeries(messages, sendAndWaitUntilMessageProcessed))
-  .then(messagesResults => {
-    console.log('messagesResults:', messagesResults)
-    result = messagesResults
-  })
+  .then(messagesResults => { result = messagesResults })
   .then(() => {
     console.log('Close the receiver...')
     receiver.detach()
     return new Promise((resolve, reject) => receiver.on('detached', () => resolve()))
   })
   .then(() => {
+    console.log('Close the connection...')
     const client = consumeMessages.__get__('client')
     client.disconnect()
   })
