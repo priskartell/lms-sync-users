32f7d0a6b2b9c8320ad842b07dd63d5204daebe9
Elena Rakhimova <elenara@kth.se>
2017-03-15 15:40:47 +0100

    Script for fetching reregistrated students is finished

--- scripts/getReregisteredStudents.js
+++ scripts/getReregisteredStudents.js
@@ -15,25 +15,28 @@ const columns = [
   'status'
 ]
 
+function getUgGroupKey (sisId) {
+    // ugKey: ladok2.kurser.KD.1070.omregistrerade_20171'
+    // courseObjs: "sis_course_id": "HH1802VT171"
+  if (sisId === null || sisId === undefined) {
+    return
+  }
+  sisCourseId = sisId
+  const termSeason = term[sisId.substring(6, 8)]
+  const ugKey = `ladok2.kurser.${sisId.substring(0, 2)}.${sisId.substring(2, 6)}.omregistrerade_20${sisId.substring(8, 10)}${termSeason}`
+  return ldap.searchGroup(`(&(objectClass=group)(CN=${ugKey}))`)
+    .then(ldap.getUsersForMembers)
+    .then(members => Promise.mapSeries(members, (member) => csvFile.writeLine([sisCourseId, member.ugKthid, 'student', 'active'], fileName)))
+}
+
 const canvasApi = require('../canvasApi.js')
 ldap.bindLdapClient()
 .then(deleteFile)
 .then(() => csvFile.writeLine(columns, fileName))
-// canvasApi.listCourses().then(courses => console.log('courses', JSON.stringify(courses, null, 4)))
-.then(() => canvasApi.getCourse('HH1802VT171'))
-.then(({sis_course_id}) => {
-  // ladok2.kurser.KD.1070.omregistrerade_20171'
-  // "sis_course_id": "HH1802VT171"
-  sisCourseId = sis_course_id
-  const termSeason = term[sis_course_id.substring(6, 8)]
-  const ugKey = `ladok2.kurser.${sis_course_id.substring(0, 2)}.${sis_course_id.substring(2, 6)}.omregistrerade_20${sis_course_id.substring(8, 10)}${termSeason}`
-  console.log('ugKey', JSON.stringify(ugKey, null, 4))
-  return ugKey
-})
-.then(ugKey => ldap.searchGroup(`(&(objectClass=group)(CN=${ugKey}))`))
-.then(ldap.getUsersForMembers)
-.then(members => Promise.mapSeries(members, (member) => csvFile.writeLine([sisCourseId, member.ugKthid, 'student', 'active'], fileName)))
-.then(ldap.unbind)
+.then(canvasApi.listCourses)
+.then(courseObjs => Promise.mapSeries(courseObjs, (cO) => getUgGroupKey(cO.sis_course_id)))
+.catch(e => console.log('error', JSON.stringify(e, null, 4)))
+.finally(ldap.unbind)
 
 function deleteFile () {
   return fs.unlinkAsync(fileName)
