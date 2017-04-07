3cfd31fd9058886b89654dfea31783bbb45f0446
Emil Stenberg <esandin@gmail.com>
2017-04-07 08:29:25 +0200

    refactor

--- messages/handleCourseMessage.js
+++ messages/handleCourseMessage.js
@@ -11,32 +11,33 @@
  const csvVol = config.full.azure.csvBlobName
  const csvDir = config.full.localFile.csvDir
 
- function _parseKey ({ug1Name, _desc}) {
+ function parseKey ({ug1Name, _desc}) {
    const {userType} = _desc
 
    if (userType === type.students) {
-     return Promise.resolve(ugParser.parseKeyStudent(ug1Name))
+     return ugParser.parseKeyStudent(ug1Name)
    }
 
    if (userType === type.teachers || userType === type.assistants || userType === type.courseresponsibles) {
-     return Promise.resolve(ugParser.parseKeyTeacher(ug1Name))
+     return ugParser.parseKeyTeacher(ug1Name)
    }
 
    log.error('Course code not parsable from Key. type, ', userType + ' ug1Name, ' + ug1Name)
-   return Promise.reject(Error('Key parse error, type, ' + userType + ' ug1Name, ' + ug1Name))
+   throw new Error('Key parse error, type, ' + userType + ' ug1Name, ' + ug1Name)
  }
 
- function process (msg) {
+ function handleCourseMessage (msg) {
    let sisCourseCodeFunction
    if (msg._desc.userType === type.omregistrerade) {
      log.info('using calcSisForOmregistrerade')
      sisCourseCodeFunction = calcSisForOmregistrerade
    } else {
-     log.info('using _parseKey')
-     sisCourseCodeFunction = _parseKey
+     log.info('using parseKey')
+     sisCourseCodeFunction = parseKey
    }
 
-   return sisCourseCodeFunction(msg)
+   return Promise.resolve()
+   .then(() => sisCourseCodeFunction(msg))
     .then(sisCourseCode => createCsvFile(msg, sisCourseCode, csvDir, csvVol))
     .then(({name}) => canvasApi.sendCsvFile(name, true))
     .then(canvasReturnValue => {
@@ -44,4 +45,4 @@
     })
  }
 
- module.exports = process
+ module.exports = {handleCourseMessage, parseKey}
--- test/unit/consumeMessagesTests.js
+++
@@ -1,6 +0,0 @@
-const test = require('blue-tape')
-require('../../consumeMessages.js')
-test('timing test', function (t) {
-  t.pass('')
-  t.end()
-})
