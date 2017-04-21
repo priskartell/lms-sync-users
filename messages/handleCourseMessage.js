698f35d4196afb6838eb892d79e491f180fe4d8e
Emil Stenberg <emilsten@kth.se>
2017-04-21 10:46:10 +0200

    format code

--- messages/handleCourseMessage.js
+++ messages/handleCourseMessage.js
@@ -12,31 +12,31 @@ const csvVol = config.full.azure.csvBlobName
 const csvDir = config.full.localFile.csvDir
 
 function parseKey ({ug1Name, _desc}) {
- const {userType} = _desc
+  const {userType} = _desc
 
- if (userType === type.students) {
-   return ugParser.parseKeyStudent(ug1Name)
- }
+  if (userType === type.students) {
+    return ugParser.parseKeyStudent(ug1Name)
+  }
 
- if (userType === type.teachers || userType === type.assistants || userType === type.courseresponsibles) {
-   return ugParser.parseKeyTeacher(ug1Name)
- }
+  if (userType === type.teachers || userType === type.assistants || userType === type.courseresponsibles) {
+    return ugParser.parseKeyTeacher(ug1Name)
+  }
 
- log.error('Course code not parsable from Key. type, ', userType + ' ug1Name, ' + ug1Name)
- throw new Error('Key parse error, type, ' + userType + ' ug1Name, ' + ug1Name)
+  log.error('Course code not parsable from Key. type, ', userType + ' ug1Name, ' + ug1Name)
+  throw new Error('Key parse error, type, ' + userType + ' ug1Name, ' + ug1Name)
 }
 
 function handleCourseMessage (msg) {
- let sisCourseCodeFunction
- if (msg._desc.userType === type.omregistrerade) {
-   log.info('using calcSisForOmregistrerade')
-   sisCourseCodeFunction = calcSisForOmregistrerade
- } else {
-   log.info('using parseKey')
-   sisCourseCodeFunction = parseKey
- }
+  let sisCourseCodeFunction
+  if (msg._desc.userType === type.omregistrerade) {
+    log.info('using calcSisForOmregistrerade')
+    sisCourseCodeFunction = calcSisForOmregistrerade
+  } else {
+    log.info('using parseKey')
+    sisCourseCodeFunction = parseKey
+  }
 
- return Promise.resolve()
+  return Promise.resolve()
   .then(() => sisCourseCodeFunction(msg))
   .then(sisCourseCode => createCsvFile(msg, sisCourseCode, csvDir, csvVol))
   .then(({name}) => canvasApi.sendCsvFile(name, true))
