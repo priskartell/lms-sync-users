8953208505b6debad4b84fd6030d51456e9fb43f
Emil Stenberg <emilsten@kth.se>
2017-06-21 13:07:12 +0200

    read azure key and canvas key from environment variables if they exist

--- canvasApi.js
+++ canvasApi.js
@@ -5,9 +5,8 @@ const apiUrl = config.secure.canvas.apiUrl || config.full.canvas.apiUrl
 
 console.log('using canvas api at:', apiUrl)
 
-const canvasApi = new CanvasApi(apiUrl, config.secure.canvas.apiKey)
+const canvasApi = new CanvasApi(apiUrl, process.env.CANVAS_API_KEY || config.secure.canvas.apiKey)
 
 canvasApi.logger = logger
 
-//
 module.exports = canvasApi
