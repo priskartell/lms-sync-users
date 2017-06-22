75f4791d1129b3f8774dd628ed3bacee9db23a59
Emil Stenberg <emilsten@kth.se>
2017-06-22 09:47:53 +0200

    one integration test passes without storing files in azure

--- messages/createCsvFile.js
+++ messages/createCsvFile.js
@@ -4,17 +4,18 @@ const config = require('../server/init/configuration')
 const Promise = require('bluebird')
 
 module.exports = function createCsvFile (msg, sisCourseCodes, csvDir, csvVol) {
-  const fileName = `${config.localFile.csvDir}enrollments.${userType}.${sisCourseCodes[0]}.${Date.now()}.csv`
+  let userType = msg._desc.userType
+
+  const fileName = `${config.full.localFile.csvDir}enrollments.${userType}.${sisCourseCodes[0]}.${Date.now()}.csv`
   // Make sure that sisCourseCodes is an array, which makes the rest of this function simpler
   if (!Array.isArray(sisCourseCodes)) {
     sisCourseCodes = [sisCourseCodes]
   }
 
-  let userType = msg._desc.userType
 
   // create one line per sisCourseId, per user. One user can be enrolled to multiple courses, for instance if this is re-registered students
   function oneLinePerSisCourseId (userId) {
-    return sisCourseCodes.forEach(sisCourseId => writeLine([sisCourseId, userId, userType, 'active']))
+    return sisCourseCodes.forEach(sisCourseId => writeLine([sisCourseId, userId, userType, 'active'], fileName))
   }
 
   return writeLine(['section_id', 'user_id', 'role', 'status'], fileName)
