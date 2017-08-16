6f3288aae35fb94bee9d9d93acad416c0d8d9116
Emil Stenberg <emilsten@kth.se>
2017-08-16 13:51:03 +0200

    created canvas script file

---
+++ scripts/add_button.js
@@ -0,0 +1,6 @@
+'use strict'
+  var but = document.createElement('a')
+  but.classList.add('ui-button')
+  but.href = '/courses/2080/external_tools/314'
+  but.innerHTML = 'Export to Ladok'
+  document.querySelector('.gradebook_menu').appendChild(but)
