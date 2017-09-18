23de9c83f3dea0ed7386799b5f3107ee59923e21
Emil Stenberg <emilsten@kth.se>
2017-09-18 09:31:50 +0200

    logging sis imports reportst

---
+++ scripts/reports/collect_sis_imports_errors.js
@@ -0,0 +1,37 @@
+process.env['NODE_ENV'] = 'production'
+const config = require('../../server/init/configuration.js')
+const CanvasApi = require('kth-canvas-api')
+const inquirer = require('inquirer')
+const moment = require('moment')
+require('dotenv').config()
+
+const canvasApi = new CanvasApi(process.env.CANVAS_API_URL, process.env.CANVAS_API_KEY)
+
+async function listErrors () {
+  try {
+    const {numOfDays} = await inquirer.prompt({
+      message: 'Hur många dagar bakåt?',
+      name: 'numOfDays',
+      type: 'number',
+      default: 1
+    })
+    console.log(numOfDays)
+    const from = moment().subtract(numOfDays, 'days').utc().toDate().toISOString()
+
+    const allSisImports = await canvasApi.recursePages(`${process.env.CANVAS_API_URL}/accounts/1/sis_imports?created_since=${from}`)
+    console.log(JSON.stringify(allSisImports, null, 4))
+
+    const flattenedSisImports = allSisImports.reduce((a,b)=>{
+      a.push(...b.sis_imports)
+      return a
+    },[])
+
+    console.log(flattenedSisImports)
+      // const canvasUsers = await canvasApi.recursePages('/accounts/1/sis_imports?created_since=2017-09-13T06:30:10.278Z')
+      // const usersWithoutSisId = canvasUsers.filter(u => !u.sis_user_id)
+      // console.log(JSON.stringify(usersWithoutSisId, null, 4))
+  } catch (e) {
+    console.error(e)
+  }
+}
+listErrors()
