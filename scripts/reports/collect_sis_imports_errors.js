479097de5efbbcb4165a8ce11b0287b89628d6d8
Emil Stenberg <emilsten@kth.se>
2017-09-18 13:57:47 +0200

    remove reading from env file

--- scripts/reports/collect_sis_imports_errors.js
+++ scripts/reports/collect_sis_imports_errors.js
@@ -1,23 +1,43 @@
 process.env['NODE_ENV'] = 'production'
+require('colors')
 const CanvasApi = require('kth-canvas-api')
 const inquirer = require('inquirer')
 const moment = require('moment')
-require('dotenv').config()
-
-const canvasApi = new CanvasApi(process.env.CANVAS_API_URL, process.env.CANVAS_API_KEY)
+const config = require('../../server/init/configuration')
 
 async function listErrors () {
   try {
+    const {apiUrl} = await inquirer.prompt(
+      {
+        message: 'Vilken miljö?',
+        name: 'apiUrl',
+        choices: [
+          {name:'prod', value:'https://kth.instructure.com/api/v1'},
+          {name:'test', value:'https://kth.test.instructure.com/api/v1'},
+          {name:'beta', value:'https://kth.beta.instructure.com/api/v1'}
+        ],
+        type: 'list'
+      })
+
+    console.log(apiUrl)
+    const {apiKey} = await inquirer.prompt({
+      message: 'Pasta in api nyckel här',
+      name: 'apiKey',
+      type: 'string'
+    })
+
+    const canvasApi = new CanvasApi(apiUrl, apiKey)
+
     const {numOfDays} = await inquirer.prompt({
       message: 'Hur många dagar bakåt?',
       name: 'numOfDays',
       type: 'number',
       default: 1
     })
-    console.log(numOfDays)
+
     const from = moment().subtract(numOfDays, 'days').utc().toDate().toISOString()
 
-    const allSisImports = await canvasApi.recursePages(`${process.env.CANVAS_API_URL}/accounts/1/sis_imports?created_since=${from}&per_page=100`)
+    const allSisImports = await canvasApi.recursePages(`${apiUrl}/accounts/1/sis_imports?created_since=${from}&per_page=100`)
 
     const flattenedSisImports = allSisImports
     .reduce((a, b) => a.concat(b.sis_imports), []) // Flatten every page
@@ -31,6 +51,7 @@ async function listErrors () {
     .filter(([fileName, warning]) => !/There were [\d,]+ more warnings/.test(warning))
     .filter(([fileName, warning]) => !warning.startsWith('An enrollment referenced a non-existent section'))
 
+    console.log('Warnings and errors:'.green)
     console.log(filteredWarnings)
   } catch (e) {
     console.error(e)
