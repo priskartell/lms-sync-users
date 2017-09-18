e5d263586df5346abc31f8ba0e65a9e7ff389dd3
Emil Stenberg <emilsten@kth.se>
2017-09-18 09:17:05 +0200

    console logging reports

--- messages/consumeMessages.js
+++ messages/consumeMessages.js
@@ -19,7 +19,6 @@ client.on('connection:disconnected', msg => log.info('connection to azure discon
 
 async function start () {
   const sharedAccessKey = process.env.AZURE_SHARED_ACCESS_KEY || config.secure.azure.SharedAccessKey
-
   log.info('connecting with the following azure url:', `amqps://${config.full.azure.SharedAccessKeyName}:${(sharedAccessKey || '').replace(/\w/g, 'x')}@${config.full.azure.host}`)
   const queueName = config.secure.azure.queueName || config.full.azure.queueName
   log.info('connecting to the queue with name ', queueName)
