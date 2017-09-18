e56d9146ef7d2eeb7b9210207a9262591c9b72d1
Emil Stenberg <emilsten@kth.se>
2017-09-18 09:56:39 +0200

    filter out warnings

--- scripts/reports/collect_sis_imports_errors.js
+++ scripts/reports/collect_sis_imports_errors.js
@@ -17,15 +17,22 @@ async function listErrors () {
     console.log(numOfDays)
     const from = moment().subtract(numOfDays, 'days').utc().toDate().toISOString()
 
-    const allSisImports = await canvasApi.recursePages(`${process.env.CANVAS_API_URL}/accounts/1/sis_imports?created_since=${from}`)
-    console.log(JSON.stringify(allSisImports, null, 4))
+    const allSisImports = await canvasApi.recursePages(`${process.env.CANVAS_API_URL}/accounts/1/sis_imports?created_since=${from}&per_page=100`)
 
-    const flattenedSisImports = allSisImports.reduce((a, b) => {
-      a.push(...b.sis_imports)
-      return a
-    }, [])
+    const flattenedSisImports = allSisImports
+    .reduce((a, b) => a.concat(b.sis_imports), []) // Flatten every page
 
-    console.log(flattenedSisImports)
+
+    const allWarnings = flattenedSisImports
+    .map(_import => _import.processing_warnings)
+    .filter(warnings => warnings) // filter out nulls and undefineds
+    .reduce((a,b)=>a.concat(b),[])  // Flatten every warning from every sis_import
+
+    // console.log(allWarnings)
+    const filteredWarnings = allWarnings
+    .filter(([fileName,warning])=> !warning.startsWith('Neither course nor section existed'))
+    .filter(([fileName,warning])=> !warning.startsWith('An enrollment referenced a non-existent section'))
+    console.log(filteredWarnings)
       // const canvasUsers = await canvasApi.recursePages('/accounts/1/sis_imports?created_since=2017-09-13T06:30:10.278Z')
       // const usersWithoutSisId = canvasUsers.filter(u => !u.sis_user_id)
       // console.log(JSON.stringify(usersWithoutSisId, null, 4))
