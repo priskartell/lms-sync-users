94f6f2a095f41b5b968af2fb00d2b8deedf1523f
Emil Stenberg <esandin@gmail.com>
2017-11-15 09:28:43 +0100

    created temporary file to get lint errors

---
+++ scripts/getLintErrors.js
@@ -0,0 +1,11 @@
+// if (true) {
+//   const foo = 1 // Inner scope of variable
+// }
+// console.log(foo)
+
+const bar = 1
+console.log(bar___) // type wrong name of variable
+
+// if ('' == ' ') {
+//   // 'Two equal signs is not good. should be three'
+// }
