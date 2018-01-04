99c6f5840e4903b9ab5d04ad73569f3dc1d3e7b1
Emil Stenberg <emilsten@kth.se>
2018-01-04 16:01:53 +0100

    log with logger

--- canvasApi.js
+++ canvasApi.js
@@ -3,7 +3,7 @@ const logger = require('./server/logging')
 const CanvasApi = require('kth-canvas-api')
 const apiUrl = config.canvas.apiUrl || config.canvas.apiUrl
 
-console.log('using canvas api at:', apiUrl)
+logger.info('using canvas api at:', apiUrl)
 
 const canvasApi = new CanvasApi(apiUrl, process.env.CANVAS_API_KEY || config.canvas.apiKey)
 
