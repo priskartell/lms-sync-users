1b6a3b01e4ae48e8d001c75add17e8a4d05dbebf
Emil Stenberg <emilsten@kth.se>
2018-01-04 15:56:59 +0100

    refactor config to follow the latest kth node templates

---
+++ server/logging.js
@@ -0,0 +1,53 @@
+const config = require('../config/serverSettings')
+const packageFile = require('../package.json')
+
+const bunyan = require('bunyan')
+
+function init (extraConfiguration) {
+  const logConf = {
+    name: 'node-logger',
+    app: packageFile.name,
+    env: environment,
+    level: configuration.log.level,
+    src: configuration.log.src,
+    ...extraConfiguration
+  }
+  return bunyan.createLogger(logConf)
+}
+
+const configuration = config.logging
+const environment = config.env
+
+// Use 'let' so we can create other instances instead of this one
+let logger = init()
+
+/*
+Wrap Bunyans log functions so we
+can create new Bunyan instances without the calling code having to bother
+about which instance to use, but the 'src' configuration logs the calling code,
+instead of logging lines in this file
+*/
+module.exports = {
+  init (extraConfiguration) {
+    logger.trace('initializing log with settings', extraConfiguration)
+    logger = init(extraConfiguration)
+  },
+  get trace () {
+    return logger.trace.bind(logger)
+  },
+  get debug () {
+    return logger.debug.bind(logger)
+  },
+  get info () {
+    return logger.info.bind(logger)
+  },
+  get warn () {
+    return logger.warn.bind(logger)
+  },
+  get error () {
+    return logger.error.bind(logger)
+  },
+  get fatal () {
+    return logger.fatal.bind(logger)
+  }
+}
