a87f37641b0d8b6e66940d3c75130a74866c489d
Emil Stenberg <emilsten@kth.se>
2018-09-04 10:49:11 +0200

    added failing integration test

--- test/integration/enrollment.test.js
+++ test/integration/enrollment.test.js
@@ -21,7 +21,7 @@ async function processMessage (message, course) {
   }
 }
 
-test.skip('should enroll an assistant in an existing course in canvas', t => {
+test('should enroll an assistant in an existing course in canvas', t => {
   t.assert(false)
 })
 test('should enroll an assistant in an existing course in canvas', t => {
